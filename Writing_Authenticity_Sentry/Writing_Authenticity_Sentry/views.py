bc61f18d323ab6d8b86b309a139503d0f1ac61ff
HasanCan6241 <mazgirtbjk62@gmail.com>
2024-02-23 22:39:05 +0300

    projemin amacı yazılan makalenin insan tarafından mı yoksa yapay zeka tarafından mı yazıldığını tespit etmektir.
    projemin amacı yazılan makalenin insan tarafından mı yoksa yapay zeka tarafından mı yazıldığını tespit etmektir.

---
+++ Writing_Authenticity_Sentry/Writing_Authenticity_Sentry/views.py
@@ -0,0 +1,44 @@
+import re
+import nltk
+import pickle
+from django.shortcuts import render
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+def anasayfa(request):
+    return render(request, 'anasayfa.html')
+
+
+with open('logistic_regression_model.pkl', 'rb') as file:
+    loaded_model = pickle.load(file)
+with open('vectorizer.pkl', 'rb') as file:
+    loaded_vectorizer = pickle.load(file)
+
+def intihal_tespit(request):
+    if request.method == 'POST':
+        text = request.POST.get('text', '')
+        text = text.lower()
+        text = re.sub(r'[^\w\s]', '', text)
+        text = re.sub(r'\d+', '', text)
+
+        stop_words = set(stopwords.words('english'))
+
+        words = [word for word in text.split() if word not in stop_words]
+
+        lemmatizer = WordNetLemmatizer()
+
+        words_lemmatized = [lemmatizer.lemmatize(word) for word in words]
+
+        processed_text = ' '.join(words_lemmatized)
+
+
+        X_new = loaded_vectorizer.transform([processed_text])
+        predictions = int(loaded_model.predict(X_new))
+
+        return render(request, 'intihal_tespit.html', {'text': text, 'predictions': predictions})
+
+    return render(request, 'intihal_tespit.html')
+
+def hakkımızda(reguest):
+        return render(reguest,'hakkımızda.html')
---
+++ Writing_Authenticity_Sentry/Writing_Authenticity_Sentry/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Writing_Authenticity_Sentry project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Writing_Authenticity_Sentry.settings')
+
+application = get_wsgi_application()
